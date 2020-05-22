ab9c03d97193609a84591bee5981407eca47d218
dackow <waldekd@softsystem.pl>
2020-05-22 23:04:11 +0200

    Comment removed

--- WCFTestService/DEV1Serice/App_Code/Service.cs
+++ WCFTestService/DEV1Serice/App_Code/Service.cs
@@ -8,7 +8,6 @@ using System.ServiceModel.Web;
 using System.Text;
 using Newtonsoft.Json;
 
-// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
 public class Service : IDEV1Service
 {
 	static string s = @"<NewDataSet></NewDataSet>";
